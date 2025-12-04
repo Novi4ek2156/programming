a1df87817c457c87a753b7ffd932a5c26fba0e6d
Novi4ek2156 <vlad_semyon@bk.ru>
2025-12-04 10:13:35 +0700

    add vscode

--- hello.cc
+++
@@ -1,6 +0,0 @@
-#include <iostream> // Include the iostream library for input/output operations
-
-int main() { // The main function, the entry point of every C++ program
-    std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the console
-    return 0; // Indicate successful program execution
-}
